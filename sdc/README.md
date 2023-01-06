37e6fb2b898a35b377f28ef22419cad12c456b87
Theodore (Alex) Evans <varmfskii@gmail.com>
2023-01-06 13:59:55 -0500

    tests 1-12 pass, added test13

--- sdc/README.md
+++ sdc/README.md
@@ -1,20 +1,20 @@
 # libsdc
 
-An access library for the cocosdc (not working)
+An access library for the cocosdc
 
 ## Functions
 
 * sdc_enable - working
 * sdc_disable - working
-* sdc_delete - untested (probably working)
+* sdc_delete - working
 * sdc_dir_get - working
 * sdc_dir_init - working
-* sdc_dir_make - untested (probably working)
+* sdc_dir_make - working
 * sdc_dir_page - working
-* sdc_dir_set - untested (probably working)
+* sdc_dir_set - working
 * sdc_img_info - working
-* sdc_img_mount - untested (probably working)
-* sdc_img_new - untested (probably working)
+* sdc_img_mount - working
+* sdc_img_new - working
 * sdc_img_size - working
 * sdc_lsec_rx - working
 * sdc_lsec_tx - untested
---
+++ sdc/libsdc/test13.asm
@@ -0,0 +1,79 @@
+	section code
+	include libsdc_h.asm
+	include decb.asm
+	export start
+start:
+	;; enable
+	ldx #msg1
+	bsr write
+	lbsr sdc_enable
+	bne error
+	bsr ok
+	;; lsec rx
+	ldx #msg3
+	bsr write
+	ldu #$0500
+	clra			; drive 0
+	clrb			; image is smaller than 64k sectors
+	ldx #17*18+2		; sector 3 track 17 (first sector of dir)
+	lbsr sdc_lsec_rx
+	bne error
+	bsr ok
+	bsr downcase
+	;; lsec tx
+	ldx #msg4
+	bsr write
+	ldu #$0500
+	clra			; drive 0
+	clrb			; image is smaller than 64k sectors
+	ldx #17*18+2		; sector 3 track 17 (first sector of dir)
+	lbsr sdc_lsec_tx
+	bne error
+	bsr ok
+	;; disable
+	ldx #msg2
+	bsr write
+	lbsr sdc_disable
+	bne error
+ok:	
+	ldx #pass
+	bra write
+error:
+	ldx #fail
+write:	
+	lda ,x+
+	beq return
+	jsr [CHROUT]
+	bra write
+return:	
+	rts
+
+downcase:
+	ldu #$04ff
+loop0@:
+	ldb #8
+loop1@:
+	lda b,u
+	cmpa #'A'
+	blt next@
+	cmpa #'Z'
+	blt next@
+	ora #32
+	sta b,u
+next@:
+	decb
+	bne loop1@
+	leau 32,u
+	cmpu #$05ff
+	bne loop0@:
+	rts
+
+pass:	.ascii "OK"
+	fcb cr,0
+fail:	.ascii "ERROR"
+	fcb cr,0
+msg1:	.asciz "ENABLE "
+msg2:	.asciz "DISABLE "
+msg3:	.asciz "LSEC RX "
+	
+	endsection
